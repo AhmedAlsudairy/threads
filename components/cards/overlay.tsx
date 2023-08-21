7a303097f15a787609b8ecb8a17441de04b9bd4d
Ahmed <gfg59204@gmail.com>
2023-08-21 06:25:32 -0400

    change heigh  asp

--- components/cards/overlay.tsx
+++ components/cards/overlay.tsx
@@ -19,7 +19,7 @@ function ImageOverlay({ src, alt }:overlayProps) {
   
     return (
       <>
-        <Image src={src} alt={alt} onClick={handleOpen} width={55} height={55} className="w-full h-full aspect-square object-contain border rounded-lg gap-3" />
+        <Image src={src} alt={alt} onClick={handleOpen} width={75} height={75} className="w-full h-full aspect-square object-contain border rounded-lg gap-3" />
         {isOpen && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center border rounded-lg z-30" onClick={handleClose}>
             <div className="relative">
