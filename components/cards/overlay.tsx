3ec0cc219e58d4d9eaf0a83a6e7ed3131cc6aaf3
Ahmed <gfg59204@gmail.com>
2023-08-21 06:17:57 -0400

    add

--- components/cards/overlay.tsx
+++ components/cards/overlay.tsx
@@ -21,7 +21,7 @@ function ImageOverlay({ src, alt }:overlayProps) {
       <>
         <Image src={src} alt={alt} onClick={handleOpen} width={55} height={55} className="w-full h-full aspect-square object-contain border rounded-lg gap-3" />
         {isOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center border rounded-lg" onClick={handleClose}>
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center border rounded-lg z-30" onClick={handleClose}>
             <div className="relative">
               <button
                 className="absolute top-0 right-0 p-2 text-white"
@@ -29,7 +29,7 @@ function ImageOverlay({ src, alt }:overlayProps) {
               >
                 <X size={15} color="black"/>
               </button>
-              <Image width={500} height={500} src={src} alt={alt}   />
+              <Image width={500} height={500} src={src} alt={alt}  layout="responsive" />
             </div>
           </div>
         )}
